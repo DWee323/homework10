e6bcc2453ee2ece72e7059539e0bb47384a91980
Diane Wee <hwee1@id.iit.edu>
2018-11-08 23:31:53 -0600

    current and all the forecast work - submit as of now

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 //https://github.com/karbassi/idx-528/blob/master/week-10/assignment/README.md
 
 //why the heck is temp in kelvin?
-//48K − 273.15 = -225.1°C
+//K − 273.15 = °C
 
 let city_search = document.querySelector("#city-search");
-console.log(city_search);
 
 const get_weather_current = (data) => {
     let weather_temp_now_current = (data.main.temp - 273.15).toFixed(2);
@@ -13,8 +12,6 @@ const get_weather_current = (data) => {
     let weather_temp_max_current =
         (data.main.temp_max - 273.15).toFixed(2);
     let weather_temp_min_current = (data.main.temp_min - 273.15).toFixed(2);
-
-    console.log(weather_temp_now_current, weather_summary_current, weather_icon_current, weather_temp_max_current, weather_temp_min_current);
 
     let on_page_temp_now_current = document.getElementById("on-page-temp-now-current");
     on_page_temp_now_current.innerText = weather_temp_now_current;
@@ -36,12 +33,11 @@ const get_weather_current = (data) => {
 //I want to forloop by (var i = 1; i <= 6; i = i + 1), but idk if I will have enough time. Therefore I will list out all separately first. If I have time afterwardsm I will study the forloop.
 const get_weather_forecast_1 = (data) => {
 
-    let weather_summary_forecast_1 = data.list[8 * 1 - 1].weather[0].main;
-    let weather_icon_forecast_1 = data.list[8 * 1 - 1].weather[0].icon;
-    let weather_temp_max_forecast_1 = (data.list[8 * 1 - 1].main.temp_max - 273.15).toFixed(2);
-    let weather_temp_min_forecast_1 = (data.list[8 * 1 - 1].main.temp_min - 273.15).toFixed(2);
-    console.log(weather_summary_forecast_1, weather_icon_forecast_1, weather_temp_max_forecast_1, weather_temp_min_forecast_1);
-
+    let weather_summary_forecast_1 = data.list[8 * 0].weather[0].main;
+    let weather_icon_forecast_1 = data.list[8 * 0].weather[0].icon;
+    let weather_temp_max_forecast_1 = (data.list[8 * 0].main.temp_max - 273.15).toFixed(2);
+    let weather_temp_min_forecast_1 = (data.list[8 * 0].main.temp_min - 273.15).toFixed(2);
+  
     let on_page_summary_forecast_1 = document.getElementById("on-page-summary-forecast-1");
     on_page_summary_forecast_1.innerText = weather_summary_forecast_1;
 
@@ -57,17 +53,16 @@ const get_weather_forecast_1 = (data) => {
 
 const get_weather_forecast_2 = (data) => {
 
-    let weather_summary_forecast_2 = data.list[8 * 2 - 1].weather[0].main;
-    let weather_icon_forecast_2 = data.list[8 * 2 - 1].weather[0].icon;
-    let weather_temp_max_forecast_2 = (data.list[8 * 2 - 1].main.temp_max - 273.15).toFixed(2);
-    let weather_temp_min_forecast_2 = (data.list[8 * 2 - 1].main.temp_min - 273.15).toFixed(2);
-    console.log(weather_summary_forecast_2, weather_icon_forecast_2, weather_temp_max_forecast_2, weather_temp_min_forecast_2);
-
+    let weather_summary_forecast_2 = data.list[8 * 1].weather[0].main;
+    let weather_icon_forecast_2 = data.list[8 * 1].weather[0].icon;
+    let weather_temp_max_forecast_2 = (data.list[8 * 1].main.temp_max - 273.15).toFixed(2);
+    let weather_temp_min_forecast_2 = (data.list[8 * 1].main.temp_min - 273.15).toFixed(2);
+   
     let on_page_summary_forecast_2 = document.getElementById("on-page-summary-forecast-2");
     on_page_summary_forecast_2.innerText = weather_summary_forecast_2;
 
     let on_page_icon_forecast_2 = document.getElementById("on-page-icon-forecast-2");
-    on_page_icon_forecast_2.src = `http://openweathermap.org/img/w/${weather_icon_forecast_2}.png`; //x.src=imgurl
+    on_page_icon_forecast_2.src = `http://openweathermap.org/img/w/${weather_icon_forecast_2}.png`; 
 
     let on_page_temp_max_forecast_2 = document.getElementById("on-page-temp-max-forecast-2");
     on_page_temp_max_forecast_2.innerHTML = weather_temp_max_forecast_2;
@@ -76,42 +71,110 @@ const get_weather_forecast_2 = (data) => {
     on_page_temp_min_forecast_2.innerHTML = weather_temp_min_forecast_2;
 };
 
+const get_weather_forecast_3 = (data) => {
 
-const searchInput = (event) => {
-    //this is one forecast: will hae to forloop and do separate for current
+    let weather_summary_forecast_3 = data.list[8 * 2].weather[0].main;
+    let weather_icon_forecast_3 = data.list[8 * 2].weather[0].icon;
+    let weather_temp_max_forecast_3 = (data.list[8 * 2].main.temp_max - 273.15).toFixed(2);
+    let weather_temp_min_forecast_3 = (data.list[8 * 2].main.temp_min - 273.15).toFixed(2);
+   
+    let on_page_summary_forecast_3 = document.getElementById("on-page-summary-forecast-3");
+    on_page_summary_forecast_3.innerText = weather_summary_forecast_3;
 
-    console.log(event); //페이지에서 빈칸에 뭔가 쓰고서 tab을 눌러야 그 창에서 나와서 change로 읽혀서 콘솔에 뭔가 로그가 돼
+    let on_page_icon_forecast_3 = document.getElementById("on-page-icon-forecast-3");
+    on_page_icon_forecast_3.src = `http://openweathermap.org/img/w/${weather_icon_forecast_3}.png`;
+
+    let on_page_temp_max_forecast_3 = document.getElementById("on-page-temp-max-forecast-3");
+    on_page_temp_max_forecast_3.innerHTML = weather_temp_max_forecast_3;
+
+    let on_page_temp_min_forecast_3 = document.getElementById("on-page-temp-min-forecast-3");
+    on_page_temp_min_forecast_3.innerHTML = weather_temp_min_forecast_3;
+};
+
+const get_weather_forecast_4 = (data) => {
+
+    let weather_summary_forecast_4 = data.list[8 * 3].weather[0].main;
+    let weather_icon_forecast_4 = data.list[8 * 3].weather[0].icon;
+    let weather_temp_max_forecast_4 = (data.list[8 * 3].main.temp_max - 273.15).toFixed(2);
+    let weather_temp_min_forecast_4 = (data.list[8 * 3].main.temp_min - 273.15).toFixed(2);
+   
+    let on_page_summary_forecast_4 = document.getElementById("on-page-summary-forecast-4");
+    on_page_summary_forecast_4.innerText = weather_summary_forecast_4;
+
+    let on_page_icon_forecast_4 = document.getElementById("on-page-icon-forecast-4");
+    on_page_icon_forecast_4.src = `http://openweathermap.org/img/w/${weather_icon_forecast_4}.png`; 
+
+    let on_page_temp_max_forecast_4 = document.getElementById("on-page-temp-max-forecast-4");
+    on_page_temp_max_forecast_4.innerHTML = weather_temp_max_forecast_4;
+
+    let on_page_temp_min_forecast_4 = document.getElementById("on-page-temp-min-forecast-4");
+    on_page_temp_min_forecast_4.innerHTML = weather_temp_min_forecast_4;
+};
+
+const get_weather_forecast_5 = (data) => {
+
+    let weather_summary_forecast_5 = data.list[8 * 4].weather[0].main;
+    let weather_icon_forecast_5 = data.list[8 * 4].weather[0].icon;
+    let weather_temp_max_forecast_5 = (data.list[8 * 4].main.temp_max - 273.15).toFixed(2);
+    let weather_temp_min_forecast_5 = (data.list[8 * 4].main.temp_min - 273.15).toFixed(2);
+   
+    let on_page_summary_forecast_5 = document.getElementById("on-page-summary-forecast-5");
+    on_page_summary_forecast_5.innerText = weather_summary_forecast_5;
+
+    let on_page_icon_forecast_5 = document.getElementById("on-page-icon-forecast-5");
+    on_page_icon_forecast_5.src = `http://openweathermap.org/img/w/${weather_icon_forecast_5}.png`; 
+
+    let on_page_temp_max_forecast_5 = document.getElementById("on-page-temp-max-forecast-5");
+    on_page_temp_max_forecast_5.innerHTML = weather_temp_max_forecast_5;
+
+    let on_page_temp_min_forecast_5 = document.getElementById("on-page-temp-min-forecast-5");
+    on_page_temp_min_forecast_5.innerHTML = weather_temp_min_forecast_5;
+};
+
+const search_input = (event) => {
+   
     let element = event.target;
-    let userInput = element.value;
-    console.log(userInput); //whatever the user types in anything here, that anything is console.logged
-
-
-    let URL_current = `http://api.openweathermap.org/data/2.5/weather?q=${userInput}&APPID=25bc90a1196e6f153eece0bc0b0fc9eb`; //this is weathered
-    console.log(URL_current);
+    let user_input = element.value;
+ 
+    let URL_current = `http://api.openweathermap.org/data/2.5/weather?q=${user_input}&APPID=25bc90a1196e6f153eece0bc0b0fc9eb`; 
+    
     fetch(URL_current) //we have a string here
         .then((response) => {
             return response.json();
-        }) //now we have an abject
+        }) //now we have an object
         .then(get_weather_current)
 
+    let URL_forecast = `http://api.openweathermap.org/data/2.5/forecast?q=${user_input}&APPID=25bc90a1196e6f153eece0bc0b0fc9eb`; 
 
-
-    let URL_forecast = `http://api.openweathermap.org/data/2.5/forecast?q=${userInput}&APPID=25bc90a1196e6f153eece0bc0b0fc9eb`; //this is weathered
-
-
-    console.log(URL_forecast);
-    fetch(URL_forecast) //we have a string here
+    fetch(URL_forecast) 
         .then((response) => {
             return response.json();
-        }) //now we have an abject
+        })
         .then(get_weather_forecast_1)
 
-    fetch(URL_forecast) //we have a string here
+    fetch(URL_forecast) 
         .then((response) => {
             return response.json();
-        }) //now we have an abject
+        }) 
         .then(get_weather_forecast_2)
 
+    fetch(URL_forecast) 
+        .then((response) => {
+            return response.json();
+        })
+        .then(get_weather_forecast_3)
+
+    fetch(URL_forecast) 
+        .then((response) => {
+            return response.json();
+        }) 
+        .then(get_weather_forecast_4)
+
+    fetch(URL_forecast) 
+        .then((response) => {
+            return response.json();
+        }) 
+        .then(get_weather_forecast_5)
 };
 
-city_search.addEventListener("change", searchInput); //mdn 에서 event reference, change 검색해봐
+city_search.addEventListener("change", search_input);
